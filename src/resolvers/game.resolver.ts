a3ad5ad422b6b263b07e337608f5376d12a7c3a8
Kenshi Shiode <atemu1212@yahoo.co.jp>
2020-05-19 22:43:35 +0900

    Add pessimistic lock

--- src/resolvers/game.resolver.ts
+++ src/resolvers/game.resolver.ts
@@ -1,40 +1,20 @@
-import { DeckCardService } from './../services/deck.card.service';
 import { GameService } from './../services/game.service';
 import { AuthGuard } from './../guards/auth.guard';
 import { Resolver, Mutation, Args } from '@nestjs/graphql';
-import {
-  UseGuards,
-  BadRequestException,
-  HttpException,
-  HttpStatus,
-} from '@nestjs/common';
+import { UseGuards } from '@nestjs/common';
 import { auth } from 'firebase-admin';
 import { User } from 'src/decorators/user.decorator';
 
 @Resolver()
 @UseGuards(AuthGuard)
 export class GameResolver {
-  private static get MIN_COUNT() {
-    return 40;
-  }
-
-  constructor(
-    private readonly gameService: GameService,
-    private readonly deckCardService: DeckCardService,
-  ) {}
+  constructor(private readonly gameService: GameService) {}
 
   @Mutation()
   async startGame(
     @User() user: auth.DecodedIdToken,
     @Args('deckId') deckId: number,
   ) {
-    const deckCards = await this.deckCardService.findByDeckId(deckId);
-    if (deckCards.length > 0 && deckCards[0].deck.userId !== user.uid) {
-      throw new HttpException('Forbidden', HttpStatus.FORBIDDEN);
-    }
-    if (deckCards.length < GameResolver.MIN_COUNT) {
-      throw new BadRequestException('Min Count');
-    }
     return await this.gameService.start(user.uid, deckId);
   }
 }
--- src/services/game.service.ts
+++ src/services/game.service.ts
@@ -1,7 +1,13 @@
+import { DeckCardEntity } from './../entities/deck.card.entity';
 import { PlayingUser } from './../graphql/index';
 import { PlayerEntity } from './../entities/player.entity';
 import { GameEntity } from './../entities/game.entity';
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  BadRequestException,
+  HttpStatus,
+  HttpException,
+} from '@nestjs/common';
 import { Repository, Connection, EntityRepository } from 'typeorm';
 import { Status } from 'src/graphql';
 
@@ -11,18 +17,44 @@ export class GameRepository extends Repository<GameEntity> {}
 @EntityRepository(PlayerEntity)
 export class PlayerRepository extends Repository<PlayerEntity> {}
 
+@EntityRepository(DeckCardEntity)
+export class DeckCardRepository extends Repository<DeckCardEntity> {}
+
 @Injectable()
 export class GameService {
+  private static get MIN_COUNT() {
+    return 40;
+  }
+
   constructor(private connection: Connection) {}
 
   async start(userId: string, deckId: number) {
     return this.connection.transaction(async manager => {
+      const deckCardRepository = manager.getCustomRepository(
+        DeckCardRepository,
+      );
       const gameRepository = manager.getCustomRepository(GameRepository);
       const playerRepository = manager.getCustomRepository(PlayerRepository);
 
-      const gameEntity = await gameRepository.findOne({
-        where: { status: Status.WAIT },
-      });
+      const deckCards = await deckCardRepository
+        .createQueryBuilder('deckCards')
+        .setLock('pessimistic_read')
+        .select()
+        .where('deckCards.deckId = :deckId', { deckId })
+        .getMany();
+      if (deckCards.length > 0 && deckCards[0].deck.userId !== userId) {
+        throw new HttpException('Forbidden', HttpStatus.FORBIDDEN);
+      }
+      if (deckCards.length < GameService.MIN_COUNT) {
+        throw new BadRequestException('Min Count');
+      }
+
+      const gameEntity = await gameRepository
+        .createQueryBuilder('games')
+        .setLock('pessimistic_read')
+        .select()
+        .where('games.status = :status', { status: Status.WAIT })
+        .getOne();
 
       // If waiting game does not exist, create new game
       if (gameEntity === undefined) {
