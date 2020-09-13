8c6bfcec294f320dc47bea5d5766d58908246b31
Kenshi Shiode <atemu1212@yahoo.co.jp>
2020-09-13 15:28:22 +0900

    Create validation files

--- src/actions/action.validatior.ts
+++ src/actions/action.validatior.ts
@@ -1,7 +1,8 @@
 import { DispatchGameActionInput } from '../graphql/index';
 import { ActionType } from '../graphql/index';
 import { GameEntity } from '../entities/game.entity';
-import { Injectable, BadRequestException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
+import { validateStartDrawTime } from './validations/start.draw.time.validation';
 
 @Injectable()
 export class ActionValidator {
@@ -12,22 +13,9 @@ export class ActionValidator {
   ) {
     switch (data.type) {
       case ActionType.START_DRAW_TIME:
-        this.validateStartDrawTime(grantedGameEntity, userId);
+        validateStartDrawTime(grantedGameEntity, userId);
       default:
         break;
-    }
-  }
-
-  private validateStartDrawTime(gameEntity: GameEntity, userId: string) {
-    const yourGameUserIndex = gameEntity.gameUsers.findIndex(
-      value => value.userId === userId,
-    );
-    if (
-      !gameEntity.gameUsers[yourGameUserIndex].actionTypes.includes(
-        ActionType.START_DRAW_TIME,
-      )
-    ) {
-      throw new BadRequestException();
     }
   }
 }
---
+++ src/actions/validations/start.draw.time.validation.ts
@@ -0,0 +1,16 @@
+import { ActionType } from './../../graphql/index';
+import { GameEntity } from './../../entities/game.entity';
+import { BadRequestException } from '@nestjs/common';
+
+export function validateStartDrawTime(gameEntity: GameEntity, userId: string) {
+  const yourGameUserIndex = gameEntity.gameUsers.findIndex(
+    value => value.userId === userId,
+  );
+  if (
+    !gameEntity.gameUsers[yourGameUserIndex].actionTypes.includes(
+      ActionType.START_DRAW_TIME,
+    )
+  ) {
+    throw new BadRequestException();
+  }
+}
